00c5cea209d3d854362bfa69190523784a2ba76c
Bartłomiej Grela <bartlomiej.grela@gmail.com>
2019-04-28 21:08:03 +0200

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="home">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+                </div>
+                <div class="col-md-6">
+                    <Timer/>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import HelloWorld from '@/components/HelloWorld.vue';
+    import Timer from '@/components/Timer.vue';
+    import BootstrapVue from 'bootstrap-vue';
+    import 'bootstrap/dist/css/bootstrap.css';
+    import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+    export default {
+        name: 'home',
+        components: {
+            HelloWorld, Timer
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .home{
+        background: #1c2c50;
+        height:100vh;
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        align-content: center;
+    }
+</style>
