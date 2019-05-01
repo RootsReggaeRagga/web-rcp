66eb39fd34bfca36e58c5d03d9201ca34e6951b4
Bartłomiej Grela <bartlomiej.grela@gmail.com>
2019-05-01 13:20:52 +0200

    hotfix... h

--- src/components/Camera.vue
+++ src/components/Camera.vue
@@ -1,20 +1,59 @@
 <template>
-   <div id="Camera">
-      <div><video ref="video" id="video" width="640" height="480" autoplay></video></div>
-      <div><button id="snap" v-on:click="capture()">Snap Photo</button></div>
-      <canvas ref="canvas" id="canvas" width="640" height="480"></canvas>
-      <ul>
-         <li v-for="c in captures">
-            <img v-bind:src="c" height="50" />
-         </li>
-      </ul>
-   </div>
+    <div id="Camera">
+        <div>
+            <video ref="video" id="video" :width="width"
+                   :height="height"
+                   :autoplay="autoplay"
+                   :playsinline="playsinline"></video>
+        </div>
+        <div>
+            <button class="btn btn-danger btn-rounded" id="snap" v-on:click="capture()">Selfiaczek pyk!</button>
+        </div>
+        <canvas ref="canvas" id="canvas" width="640" height="480"></canvas>
+        <ul>
+            <li v-for="c in captures">
+                <img v-bind:src="c" height="50"/>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
     export default {
         name: 'Camera',
-
+        props: {
+            width: {
+                type: [Number, String],
+                default: "100%"
+            },
+            height: {
+                type: [Number, String],
+                default: 500
+            },
+            autoplay: {
+                type: Boolean,
+                default: true
+            },
+            screenshotFormat: {
+                type: String,
+                default: "image/jpeg"
+            },
+            deviceId: {
+                type: String,
+                default: null
+            },
+            playsinline: {
+                type: Boolean,
+                default: true
+            },
+            resolution: {
+                type: Object,
+                default: null,
+                validator: value => {
+                    return value.height && value.width;
+                }
+            }
+        },
         data() {
             return {
                 video: {},
@@ -24,9 +63,9 @@
         },
         mounted() {
             this.video = this.$refs.video;
-            if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-                navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
-                    this.video.src = window.URL.createObjectURL(stream);
+            if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+                navigator.mediaDevices.getUserMedia({video: true}).then(stream => {
+                    this.video.src = window.HTMLMediaElement.srcObject = stream;
                     this.video.play();
                 });
             }
@@ -34,7 +73,7 @@
         methods: {
             capture() {
                 this.canvas = this.$refs.canvas;
-                var context = this.canvas.getContext("2d").drawImage(this.video, 0, 0, 640, 480);
+                var context = this.canvas.getContext("2d").drawImage(this.video, 0, 0, this.width, this.height);
                 this.captures.push(canvas.toDataURL("image/png"));
             }
         }
@@ -45,9 +84,11 @@
     #video {
         background-color: #000000;
     }
+
     #canvas {
         display: none;
     }
+
     li {
         display: inline;
         padding: 5px;
